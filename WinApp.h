fa0b036e6364d4ceabc3d986be3b60e9a8fe7e6b
kodai <kodai@MSI>
2023-10-24 15:19:16 +0900

    03_02完成

--- WinApp.h
+++ WinApp.h
@@ -3,12 +3,27 @@
 
 class WinApp
 {
+public:
+
+	static LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 	void Initialize();
 
 	void Update();
 
+	//getter
+	HWND GetHwnd() const { return hwnd; }
+	HINSTANCE  GetHinstance() const { return w.hInstance; }
 
+public:
+	// ウィンドウサイズ
+	static const int window_width = 1280;  // 横幅
+	static const int window_height = 720;  // 縦幅
+
+private:
+
+	HWND hwnd;
+	WNDCLASSEX w{};
 
 };
 
