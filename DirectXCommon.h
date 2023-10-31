34cb153b50ef900cae00877b40e6b4240bce4bfd
kodai <kodai@MSI>
2023-10-31 15:55:16 +0900

    04_02完成

--- DirectXCommon.h
+++ DirectXCommon.h
@@ -1,18 +1,64 @@
 #pragma once
+#include <wrl.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <vector>
+
+#include "WinApp.h"
 
 
 class DirectXCommon
 {
 public:
 
-	void Initialize();
+	void Initialize(WinApp* winApp);
 
 	void Update();
 
 
 private:
 
+	//デバイス
+	void DeviceInitialize();
 
+	//コマンド
+	void CommandInitialize();
 
+	//スワップチェーン
+	void SwapChainInitialize();
+
+	//レンダーターゲット
+	void RenderTargetInitialize();
+
+	//デフス
+	void DepthBufferInitialize();
+
+	//フェンス
+	void FenceInitialize();
+
+private:
+
+	WinApp* winApp_ = nullptr;
+
+	Microsoft::WRL::ComPtr<ID3D12Device> device;
+	Microsoft::WRL::ComPtr<IDXGIFactory7> dxgiFactory;
+
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> commandAllocator;
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList;
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue;
+
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtvHeap;
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc{};
+	std::vector<ComPtr<ID3D12Resource>> backBuffers;
+
+	Microsoft::WRL::ComPtr<IDXGISwapChain4> swapChain;
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> depthBuff;
+	D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc{};
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> dsvHeap;
+
+	Microsoft::WRL::ComPtr<ID3D12Fence> fence;
+	UINT64 fenceVal = 0;
 };
 
